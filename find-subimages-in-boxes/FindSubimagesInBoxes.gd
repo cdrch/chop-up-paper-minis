fac714c2a2a21898c295952dc8197802cf9723bc
Carter David Henning <carterdavidhenning@gmail.com>
2022-01-07 20:26:15 -0500

    Switch back to testing non-rotated trouble pages

--- find-subimages-in-boxes/FindSubimagesInBoxes.gd
+++ find-subimages-in-boxes/FindSubimagesInBoxes.gd
@@ -26,7 +26,7 @@ var inner_loops_discard := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	image_directory.open("res://rotated_trouble_pages")
+	image_directory.open("res://trouble_pages")
 	image_directory.list_dir_begin(true)
 	thread = Thread.new()
 	thread.start(self, "_thread_function")
@@ -40,7 +40,7 @@ func _thread_function():
 	var next_image = image_directory.get_next()
 	while next_image != "":
 		if not next_image.ends_with("import"):
-			images.append(load("res://rotated_trouble_pages/" + next_image))
+			images.append(load("res://trouble_pages/" + next_image))
 			print(next_image)
 		next_image = image_directory.get_next()
 	# Call function
